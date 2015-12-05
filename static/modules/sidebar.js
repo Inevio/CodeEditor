eb7db30c62ff3f4859bdfaea6ef34acd5d12fd23
Javier Garmon <jgarmon@inevio.com>
2015-12-05 11:14:01 +0100

    Added sidebar example

--- static/modules/sidebar.js
+++ static/modules/sidebar.js
@@ -1,6 +1,22 @@
 
 var ITEM_HTML = '<article class="sidebar-item"><span class="sidebar-name"></span><section class="sidebar-children"></section></article>';
 
+var generateTree = function( tree ){
+
+};
+
 module.exports = function( selector ){
+
   var sidebar = $( selector );
+  var item = $(ITEM_HTML);
+  var item2 = $(ITEM_HTML);
+  var item3 = $(ITEM_HTML);
+
+  item.find('span').text('Texto');
+  item2.find('span').text('Texto 2');
+  item3.find('span').text('Texto 3');
+  sidebar.append(item);
+  sidebar.find('section').append( item2 )
+  sidebar.find('section').first().append( item3 )
+
 };
